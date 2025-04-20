fa42441de4517ececf0ecfddd30a5272e655ec94
zarte13 <phil.dlebel.eco@gmail.com>
2025-04-20 12:19:08 -0400

    fix start connection script

--- start_connections_OS1.sh
+++ start_connections_OS1.sh
@@ -1,4 +1,6 @@
-mavproxy --master=udpout:192.168.144.12:19856 --out=127.0.0.1:14550 --out=127.0.0.1:14551 --out=127.0.0.1:14552
+mavproxy --master=udpout:192.168.144.12:19856 --out=127.0.0.1:14550 --out=127.0.0.1:14551 --out=127.0.0.1:14552 &
+MAVPROXY_PID=$!
+echo "MAVProxy started with PID $MAVPROXY_PID"
 
-sleep 1
+sleep 2
 python3 param_manager.py -c udp:127.0.0.1:14552 -f ofa_ardupilot.param
